c8a63b1ca4727572b37a7f7df0b39bf29b2f29ee
Albina <albizuewa@gmail.com>
2026-03-31 15:19:21 -0700

    HTTP мок для accounts-service

--- tests/mock/http/server.py
+++ tests/mock/http/server.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 
 from tests.config import test_settings
+from tests.mock.http.api.accounts import accounts_mock_router
 from tests.mock.http.api.cards import cards_mock_router
 from tests.mock.http.api.users import users_mock_router
 
@@ -9,6 +10,7 @@ app = FastAPI(title='mock-service')
 
 app.include_router(users_mock_router)
 app.include_router(cards_mock_router)
+app.include_router(accounts_mock_router)
 
 if __name__ == '__main__':
     # HTTP mock-service runs as an independent process.
