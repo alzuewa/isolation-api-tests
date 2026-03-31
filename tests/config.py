30587d03b2e4c87eb10e3d3922b66adf2c7a21d0
Albina <albizuewa@gmail.com>
2026-03-30 23:25:50 -0700

    Конфигурация Postgres для тестового окружения

--- tests/config.py
+++ tests/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from tests.tools.config.grpc import GRPCClientTestConfig, GRPCServerTestConfig
 from tests.tools.config.http import HTTPClientTestConfig, HTTPServerTestConfig
 from tests.tools.config.kafka import KafkaClientTestConfig
+from tests.tools.config.postgres import PostgresClientTestConfig
 
 
 class TestSettings(BaseSettings):
@@ -20,6 +21,8 @@ class TestSettings(BaseSettings):
 
     mock_http_server: HTTPServerTestConfig
     mock_grpc_server: GRPCServerTestConfig
+
+    operations_postgres_client: PostgresClientTestConfig
 
     gateway_http_client: HTTPClientTestConfig
     gateway_grpc_client: GRPCClientTestConfig
