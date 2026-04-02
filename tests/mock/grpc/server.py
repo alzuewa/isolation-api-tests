521a6d878882f106513bb12c84bee927db53a106
Albina <albizuewa@gmail.com>
2026-04-01 22:15:20 -0700

    gRPC мок для accounts-service

--- tests/mock/grpc/server.py
+++ tests/mock/grpc/server.py
@@ -4,9 +4,11 @@ from concurrent import futures
 import grpc
 from grpc_reflection.v1alpha import reflection
 
+from contracts.services.accounts import accounts_service_pb2, accounts_service_pb2_grpc
 from contracts.services.cards import cards_service_pb2, cards_service_pb2_grpc
 from contracts.services.users import users_service_pb2, users_service_pb2_grpc
 from tests.config import test_settings
+from tests.mock.grpc.api.accounts import AccountsMockService
 from tests.mock.grpc.api.cards import CardsMockService
 from tests.mock.grpc.api.users import UsersMockService
 
@@ -19,6 +21,7 @@ async def serve():
 
     users_service_pb2_grpc.add_UsersServiceServicer_to_server(UsersMockService(), server)
     cards_service_pb2_grpc.add_CardsServiceServicer_to_server(CardsMockService(), server)
+    accounts_service_pb2_grpc.add_AccountsServiceServicer_to_server(AccountsMockService(), server)
 
     # Reflection is needed for debugging purposes (Postman/grpcurl).
     reflection.enable_server_reflection(
@@ -26,6 +29,7 @@ async def serve():
             reflection.SERVICE_NAME,
             users_service_pb2.DESCRIPTOR.services_by_name['UsersService'].full_name,
             cards_service_pb2.DESCRIPTOR.services_by_name['CardsService'].full_name,
+            accounts_service_pb2.DESCRIPTOR.services_by_name['AccountsService'].full_name,
         ),
         server
     )
